e9da06a1dd7238276bd420784721859aa266d092
oussabi-tarek <tarek.oussabi@edu.uca.ma>
2023-12-08 15:36:02 +0100

    fix:fix carousel issues

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -4,7 +4,7 @@ import Carousel from './Carousel';
 const About=()=>{
     return(
         <div className='flex flex-col p-20'>
-            <div className='flex flex-col mb-4 self-center  space-y-4 ml-12'>
+            <div className='flex flex-col mb-4 self-center  space-y-4 '>
                 <p className='font-serif self-center '>La grande place du centre hospitalier médical</p>
                 <p className='font-sans text-sm self-center font-bold text-indigo-700'>
                 Nous fournissons des astuces et des conseils spéciaux en matière de soins de santé et de haut niveau de la meilleure technologie impliquée dans notre hôpital.

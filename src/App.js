3b8353cfbe3a210a59812d5a17c332cd49c2e5dd
oussabi-tarek <tarek.oussabi@edu.uca.ma>
2023-12-08 11:04:51 +0100

    feat:home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 import "./App.css";
 import AuthPage from "./components/auth/AuthPage";
+import Home from "./components/home/Home";
 
 
 function App() {
   return(
-    <AuthPage />
+    <BrowserRouter>
+       <Routes>
+        <Route path="/login" element={<AuthPage />} />
+        <Route path="/" element={<Home />} />
+       </Routes>
+    </BrowserRouter>
+   
   )
 }
 
